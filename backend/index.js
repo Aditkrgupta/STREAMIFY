69502358b9271579876423e39762720f4af5a8f9
Adityakrgupta <adityakumarg876@gmail.com>
2026-09-22 23:29:45 +0530

    Add Loader.io verification and static file support

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Server } = require("socket.io");
 
 const app = express();
+app.use(express.static("public"))
 const PORT = process.env.PORT || 8000;
 
 const server = app.listen(PORT, () => {
